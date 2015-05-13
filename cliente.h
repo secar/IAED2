032b59b9567834ccc87fbab6356fee11c0f41280
Sérgio <sergio.d.carvalho@tecnico.ulisboa.pt>
2015-05-13 18:21:41 +0100

    Create cliente.h

---
+++ cliente.h
@@ -0,0 +1,7 @@
+cliente cria_cliente (unsigned long int ref, unsigned long int valor_ini, char tipo);
+
+void insere_cliente (cliente cliente_alvo);
+
+cliente seek_cliente (unsigned long int referencia_alvo);
+
+void apaga_cliente(cliente cliente_alvo);
