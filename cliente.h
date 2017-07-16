b95ec6ccf629c532d681e035c0cc3d9077aea0a2
edeak <edeak@null.net>
2017-07-16 08:34:54 +0100

    Command line reading and parsing works.

--- cliente.h
+++
@@ -1,7 +0,0 @@
-cliente cria_cliente (unsigned long int ref, unsigned long int valor_ini, char tipo);
-
-void insere_cliente (cliente cliente_alvo);
-
-cliente seek_cliente (unsigned long int referencia_alvo);
-
-void apaga_cliente(cliente cliente_alvo);
